aa0c484d477a3d665d9102388fcf0500aa7fc8fb
ho-ri1991 <hage.1991@hotmail.co.jp>
2017-12-30 20:31:42 +0900

    add header for including all type_traits

---
+++ include/hrlib/type_traits/type_traits.hpp
@@ -0,0 +1,10 @@
+#ifndef HRLIB_TYPE_TRAITS_TYPE_TRATIS
+#define HRLIB_TYPE_TRAITS_TYPE_TRATIS
+
+#include "identity.hpp"
+#include "best_match_in_set.hpp"
+#include "is_detected.hpp"
+#include "is_match_template.hpp"
+
+#endif
+
