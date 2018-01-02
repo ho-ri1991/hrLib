38b20b75c824f6994344543d3f5fd3056122f24c
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-01-02 12:26:38 +0900

    add some constructors for deducation guide and implement Result class

--- include/hrlib/error_handling/result.hpp
+++ include/hrlib/error_handling/result.hpp
@@ -4,35 +4,54 @@
 #include <string>
 #include <tuple>
 #include <functional>
+#include <variant>
 #include <hrlib/type_traits/type_traits.hpp>
 
 namespace hrlib::error_handling {
     namespace result {
+        //a type which represents normal state in the Result class
         template <typename T>
         struct Ok {
             using wrap_type = T;
-            T data;
+            wrap_type data;
+            constexpr Ok(const wrap_type& data) noexcept(std::is_nothrow_copy_constructible_v<wrap_type>): data(data){}
+            constexpr Ok(wrap_type&& data) noexcept(std::is_nothrow_move_constructible_v<wrap_type>): data(std::move(data)){}
+            Ok(const Ok&) = default;
+            Ok(Ok&&) = default;
+            Ok& operator=(const Ok&) = default;
+            Ok& operator=(Ok&&) = default;
+            ~Ok() = default;
         };
-    
+
+        //a type which represents error state in the Result class
         template <typename T>
         struct Err {
             using wrap_type = T;
             T err;
+            constexpr Err(const wrap_type& err) noexcept(std::is_nothrow_copy_constructible_v<wrap_type>): err(err){}
+            constexpr Err(wrap_type&& err) noexcept(std::is_nothrow_move_constructible_v<wrap_type>): err(std::move(err)){}
+            Err(const Err&) = default;
+            Err(Err&&) = default;
+            Err& operator=(const Err&) = default;
+            Err& operator=(Err&&) = default;
+            ~Err() = default;
         };
 
+        //a type for merging errors in Result, this type is used when we use DefaultMergePolicy
         template <typename... Ts>
         struct ErrorTuple {
             std::tuple<Ts...> errors;
-            constexpr ErrorTuple(const std::tuple<Ts...>& tuple): errors(tuple){};
-            constexpr ErrorTuple(std::tuple<Ts...>&& tuple): errors(std::move(tuple)){};
+            constexpr ErrorTuple(const std::tuple<Ts...>& tuple) noexcept(std::is_nothrow_copy_constructible_v<std::tuple<Ts...>>): errors(tuple){};
+            constexpr ErrorTuple(std::tuple<Ts...>&& tuple) noexcept(std::is_nothrow_move_constructible_v<std::tuple<Ts...>>): errors(std::move(tuple)){};
         };
 
+        //deducation guides for ErrorTuple
         template <typename... Ts>
         ErrorTuple(const std::tuple<Ts...>&)->ErrorTuple<Ts...>;
-
         template <typename... Ts>
         ErrorTuple(std::tuple<Ts...>&&)->ErrorTuple<Ts...>;
 
+        //meta function which returns is the type is an ErrorTuple
         template <typename T>
         using is_error_tuple = type_traits::is_match_template<ErrorTuple, T>;
         template <typename T>
@@ -64,6 +83,42 @@ namespace hrlib::error_handling {
 
     template <typename WrapType, typename ErrType = std::string, typename MergePolicy = result::DefaultMergePolicy>
     class Result {
+    public:
+        using ok_wrap_type = WrapType;
+        using error_wrap_type = ErrType;
+        using Ok = result::Ok<ok_wrap_type>;
+        using Err = result::Err<error_wrap_type>;
+    private:
+        std::variant<Ok, Err> var;
+    public:
+        Result(const Ok& ok) noexcept(std::is_nothrow_copy_constructible_v<Ok>): var(ok){}
+        Result(Ok&& ok) noexcept(std::is_nothrow_move_constructible_v<Ok>): var(std::move(ok)){}
+        Result(const Err& err) noexcept(std::is_nothrow_copy_constructible_v<Err>): var(err){}
+        Result(Err&& err) noexcept(std::is_nothrow_move_constructible_v<Err>): var(std::move(err)){}
+        Result(const Result&) = default;
+        Result(Result&&) = default;
+        Result& operator=(const Result&) = default;
+        Result& operator=(Result&&) = default;
+        ~Result() = default;
+    public:
+        explicit operator bool()const noexcept { return std::get_if<Ok>(&var) != nullptr; }
+        ok_wrap_type& get_ok() &{ return std::get<Ok>(var).data; }
+        const ok_wrap_type& get_ok() const &{ return std::get<Ok>(var).data; }
+        ok_wrap_type&& get_ok() &&{ return std::move(std::get<Ok>(var).data); }
+        error_wrap_type& get_err() &{ return std::get<Err>(var).err; }
+        const error_wrap_type& get_err() const &{ return std::get<Err>(var).err; }
+        error_wrap_type&& get_err() &&{ return std::move(std::get<Err>(var)).err; }
+    public:
+        template <typename Fn>
+        auto ok_or(Fn fn) const& noexcept(std::is_nothrow_copy_constructible_v<ok_wrap_type> && std::is_nothrow_invocable_r_v<ok_wrap_type, Fn>)
+            -> std::enable_if_t<std::is_invocable_r_v<ok_wrap_type, Fn>, ok_wrap_type> {
+            return (*this) ? get_ok() : fn();
+        }
+        template <typename Fn>
+        auto ok_or(Fn fn) && noexcept(std::is_nothrow_move_constructible_v<ok_wrap_type> && std::is_nothrow_invocable_r_v<ok_wrap_type, Fn>)
+            -> std::enable_if<std::is_invocable_r_v<ok_wrap_type, Fn>, ok_wrap_type> {
+            return (*this) ? std::move(get_ok()) : fn();
+        }
     };
 }
 
